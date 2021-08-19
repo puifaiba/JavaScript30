d2794fa765a8d6c905fbf1e124df962f50d85a30
puifaiba <puifai.ba@gmail.com>
2021-08-20 01:27:44 +0700

    skip function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -26,9 +26,17 @@ function updateButton() {
   toggle.textContent = icon
 }
 
+function skip() {
+  console.log(this.dataset.skip)
+  video.currentTime += parseFloat(this.dataset.skip)
+}
+
 // Hook up event listeners
 video.addEventListener("click", togglePlay)
 video.addEventListener("play", updateButton)
 video.addEventListener("pause", updateButton)
 
 toggle.addEventListener("click", togglePlay)
+skipButtons.forEach((button) => {
+  button.addEventListener("click", skip)
+})
